f621735b2fb8308ba21188db30969e80fde3bd4b
AnnaYegorova <good4anna@gmail.com>
2022-04-22 19:43:07 +0200

    add input and button

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  nameInputId = nanoid();
+  handleNameChange = e => {
+    this.setState({
+      name: e.currentTarget.value,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({ contacts: [this.state.name] });
+    this.reset();
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <p>Phonebook</p>
+          <label htmlFor={this.nameInputId}>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+              onChange={this.handleNameChange}
+              id={this.nameInputId}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <p>Contacts</p>
+        <ul>{}</ul>
+      </>
+    );
+  }
+}
